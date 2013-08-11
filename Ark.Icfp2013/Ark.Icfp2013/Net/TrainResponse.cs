71dcfc456f8792a62995c9c323cd228bc8ea1886
Alexey Volkov <ark@ark-kun.com>
2013-08-11 17:51:44 +0400

    Added TrainResponse class

---
+++ Ark.Icfp2013/Ark.Icfp2013/Net/TrainResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Ark.Icfp2013.Net {
+    [JsonObject(MemberSerialization.OptIn)]
+    class TrainResponse {
+        [JsonProperty("challenge")]
+        public string Program;
+
+        [JsonProperty("id")]
+        public string Id;
+
+        [JsonProperty("size")]
+        public int Size;
+
+        [JsonProperty("operators")]
+        public string[] Operators;
+    }
+}
