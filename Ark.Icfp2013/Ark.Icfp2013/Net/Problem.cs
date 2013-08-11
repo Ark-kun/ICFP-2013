b97427d93379b1e6219f375d353c031ab53ccf09
Alexey Volkov <ark@ark-kun.com>
2013-08-12 02:08:31 +0400

    Added the Problem class.

---
+++ Ark.Icfp2013/Ark.Icfp2013/Net/Problem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ark.Icfp2013.Net {
+    public class Problem {
+        public string ID;
+        public int Size;
+        public Dictionary<ulong, ulong> EvalResults = new Dictionary<ulong, ulong>();
+        public string Solution;
+
+        public OperatorType UsedOperators;
+    }
+}
